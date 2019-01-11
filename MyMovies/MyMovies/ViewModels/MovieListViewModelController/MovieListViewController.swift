2cbc8968e5ab095648cec5da822d1254c84a5337
Bevin Patel <bevin.patel@sparsh-technologies.co.in>
2019-01-11 14:29:44 +0530

    embad a uipage view controller of comming soon and now showing contrainer

--- MyMovies/MyMovies/ViewModels/MovieListViewModelController/MovieListViewController.swift
+++ MyMovies/MyMovies/ViewModels/MovieListViewModelController/MovieListViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MXSegmentedControl
 
 class MovieListViewController: BaseViewController {
     var viewModel : MovieListViewModel!
+    
+    @IBOutlet var segmentedControl: MXSegmentedControl!
+    var pageViewController : UIPageViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +21,24 @@ class MovieListViewController: BaseViewController {
     }
     override func setUI() {
         super.setUI()
+        segmentedControl.append(title: "Now Showing")
+            .set(title: #colorLiteral(red: 0.08598647267, green: 0.093843095, blue: 0.1104642078, alpha: 1), for: .selected)
+        segmentedControl.append(title: "Comming Soon")
+            .set(title: #colorLiteral(red: 0.08598647267, green: 0.093843095, blue: 0.1104642078, alpha: 1), for: .selected)        
+        segmentedControl.indicator.lineView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
     }
     override func setEventBinding() {
         super.setEventBinding()
     }
     override func setDataBinding() {
         super.setDataBinding()
+    }
+    @IBAction func onChangeSegment(_ sender: MXSegmentedControl) {
+        print("value chagne\(sender.selectedIndex)");
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let pageController = segue.destination as? UIPageViewController{
+            self.pageViewController = pageController;
+        }
     }
 }
